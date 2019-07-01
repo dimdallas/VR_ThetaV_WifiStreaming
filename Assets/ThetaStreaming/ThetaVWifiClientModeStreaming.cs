1df4b5a7941458a7b3edd9bd8287ed73794aa738
matsumoto_razerblade <matsumoto@cyber.t.u-tokyo.ac.jp>
2019-07-01 15:26:54 +0200

    Theta V WiFi AP/Client mode Streaming

---
+++ Assets/ThetaStreaming/ThetaVWifiClientModeStreaming.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using LitJson;
+
+public class ThetaVWifiClientModeStreaming : MonoBehaviour {
+
+	private bool isLooping = true;
+	private Renderer myRenderer;
+	public string thetaUrl = "http://192.168.1.1";
+	private string executeCmd = "/osc/commands/execute";
+    public string THETA_ID = "THETAYL00165011";
+    public string THETA_PASSWORD = "00165011";
+
+    // Use this for initialization
+    IEnumerator Start () {
+        string url = thetaUrl + executeCmd;
+
+        //Digest Auth
+        var credentialCache = new CredentialCache();
+        credentialCache.Add(
+            new System.Uri(url),
+            "Digest",
+            new NetworkCredential(THETA_ID, THETA_PASSWORD)
+            );
+
+        myRenderer = GetComponent<Renderer>();
+
+        byte[] postBytes = Encoding.Default.GetBytes("{" +
+            "\"name\" : \"camera.getLivePreview\"" +
+            "}");
+
+        var request = HttpWebRequest.Create(url);
+
+        request.PreAuthenticate = true;
+        request.Credentials = credentialCache;
+        request.Timeout = (int)(30 * 10000f);
+        request.Method = "POST";
+        System.Net.ServicePointManager.Expect100Continue = false;
+        request.ContentType = "application/json;charset=utf-8";
+
+        request.ContentLength = postBytes.Length;
+        Stream reqStream = request.GetRequestStream();
+
+        reqStream.Write(postBytes, 0, postBytes.Length);
+
+        using (var stream = request.GetResponse().GetResponseStream())
+        {
+            BinaryReader reader = new BinaryReader(new BufferedStream(stream), new System.Text.ASCIIEncoding());
+            {
+                List<byte> imageBytes = new List<byte>();
+                bool isLoadStart = false;
+                while (isLooping)
+                {
+                    byte byteData1 = reader.ReadByte();
+                    byte byteData2 = reader.ReadByte();
+
+                    if (!isLoadStart)
+                    {
+                        if (byteData1 == 0xFF && byteData2 == 0xD8)
+                        {
+                            // mjpeg start! ( [0xFF 0xD8 ... )
+                            imageBytes.Add(byteData1);
+                            imageBytes.Add(byteData2);
+
+                            isLoadStart = true;
+                        }
+                    }
+                    else
+                    {
+                        imageBytes.Add(byteData1);
+                        imageBytes.Add(byteData2);
+
+                        if (byteData1 == 0xFF && byteData2 == 0xD9)
+                        {
+                            // mjpeg end (... 0xFF 0xD9] )
+
+                            Texture2D tex = new Texture2D(2, 2);
+                            tex.LoadImage((byte[])imageBytes.ToArray());
+                            myRenderer.material.mainTexture = tex;
+                            imageBytes.Clear();
+                            yield return null;
+                            isLoadStart = false;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            isLooping = false;
+        }
+    }
+}
